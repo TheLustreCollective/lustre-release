TheLustreCollective/lustre-release

commit 510aea4a37ea26769be5946ca93a4576a1dd4095

LU-12514 utils: add "lustre_tgt" filesystem type
Add a "lustre_tgt" filesystem type separate from the "lustre"
filesystem type for forward compatibility to allow the client and
server mount code to be disentangled.  This does not actually make
any changes to the mount code itself, but allows testing interop
and fallback after an upgrade if the new filesystem type is used.

Remove the check for the "lustre_lite" filesystem type from
llapi_is_lustre_mnttype() since that was unused since Lustre 1.4,
and replace it with a check for filesystem type "lustre_tgt".

Test-Parameters: trivial testlist=conf-sanity
Change-Id: If635834dfff502e6ecf8a85a2665de09efd52372
Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
Signed-off-by: Oleg Drokin <green@whamcloud.com>
Reviewed-on: https://review.whamcloud.com/36165
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Neil Brown <neilb@suse.de>
Tested-by: Maloo <maloo@whamcloud.com>

lustre/utils/Makefile.am
@@ -23,7 +23,7 @@ GSSLIB =
 endif
 
 # mount only finds helpers in /sbin
-rootsbin_PROGRAMS = mount.lustre
+rootsbin_PROGRAMS = mount.lustre mount.lustre_tgt
 bin_SCRIPTS   = llstat llobdstat plot-llstat
 bin_PROGRAMS  = lfs
 sbin_SCRIPTS  = ldlm_debug_upcall
@@ -185,6 +185,10 @@ mount_lustre_CPPFLAGS := ${MNTMODCFLAGS}
 mount_lustre_LDFLAGS  := ${MNTMODLDFLAGS}
 mount_lustre_LDADD    := $(SELINUX) $(LDLIBMOUNT) $(PLUGIN_LIB) $(GSSLIB) \
 			 $(top_builddir)/libcfs/libcfs/libcfs.la $(MNTMODLIBS)
+mount_lustre_tgt_SOURCES = ${mount_lustre_SOURCES}
+mount_lustre_tgt_CPPFLAGS = ${mount_lustre_CPPFLAGS}
+mount_lustre_tgt_LDFLAGS = ${mount_lustre_LDFLAGS}
+mount_lustre_tgt_LDADD = ${mount_lustre_LDADD}
 
 mkfs_lustre_SOURCES = mkfs_lustre.c mount_utils.c mount_utils.h $(GSSSRC)
 mkfs_lustre_CPPFLAGS := -UTUNEFS ${MNTMODCFLAGS}

lustre.spec.in
@@ -521,6 +521,7 @@ fi
 %{_bindir}/lfs
 %{_bindir}/lfs_migrate
 /sbin/mount.lustre
+/sbin/mount.lustre_tgt
 %if %{with static}
 %{_libdir}/liblustreapi.a
 %endif

lustre/doc/Makefile.am
@@ -132,6 +132,7 @@ MANFILES =					\
 	lustre_routes_conversion.8		\
 	lustre_rsync.8				\
 	mount.lustre.8				\
+	mount.lustre_tgt.8			\
 	nids.5					\
 	plot-llstat.8				\
 	routerstat.8

lustre/doc/mount.lustre.8
@@ -5,21 +5,26 @@
 .\"
 .\" This file may be copied under the terms of the GNU Public License v2.
 .\"
-.TH mount.lustre 8 "2008 Mar 15" Lustre "configuration utilities"
+.TH mount.lustre 8 "2019 Jul 30" Lustre "configuration utilities"
 .SH NAME
-mount.lustre \- start a Lustre client or target service
+mount.lustre \- mount helper for Lustre client
+mount.lustre_tgt \- mount helper for Lustre target service
 .SH SYNOPSIS
 .br
-.BI "mount \-t lustre [\-o " options "] " "<block_device> <server_mountpoint>"
+.BI "mount -t lustre [-o " options "] " "<mgsname>:/<fsname> <client_mountpoint>"
 .br
-.BI "mount \-t lustre [\-o " options "] " "<mgsname>:/<fsname> <client_mountpoint>"
+.BI "mount -t lustre_tgt [-o " options "] " "<block_device> <server_mountpoint>"
 .SH DESCRIPTION
 .B mount.lustre
-is used to start a Lustre client or target service.  This program should not
-be called directly; rather it is a helper program invoked through
+and
+.B mount.lustre_tgt
+are used to start a Lustre client or target service, respectively.  They
+should not be called directly; rather they are helper programs invoked through
 .BR mount (8)
 as above when the
 .B lustre
+or
+.B lustre_tgt
 filesystem type is used.  Lustre clients and targets are stopped by using the
 .BR umount (8)
 command.
@@ -30,7 +35,7 @@ There are two forms for the device option passed to the
 command, depending on whether a client or a target service is started:
 .TP
 .IR <mgsname> :/ <fsname>[/<subdir>]
-mounts the Lustre filesystem named
+mounts the Lustre client filesystem named
 .I fsname
 (optionally starting at subdirectory
 .I subdir
@@ -46,11 +51,10 @@ for automatic mount at boot time, is usable like any local filesystem, and
 provides a full POSIX-compilant interface.
 .TP
 .I block_device
-starts the target service defined by the
+specifies the phyisical disk or ZFS dataset uses to start the target service
+formatted by the
 .IR mkfs.lustre (8)
-command on the physical disk or ZFS dataset given by
-.IR block_device .
-The
+command.  The
 .I block_device
 may be specified using
 .BI -L label
@@ -66,7 +70,7 @@ or similar.  The mounted target service filesystem at
 .I mountpoint
 is only useful for
 .BR df (1)
-operations and appears in
+operations to show the current space usage, and appears in
 .BR /proc/mounts
 to show the device is in use.
 .SH OPTIONS
@@ -297,17 +301,17 @@ tries the second one. On each service node, the comma-separated NIDs refer to
 different interfaces on the same host, and the Lustre client chooses the best
 one for communication based on which network interfaces are available locally.
 .TP
-.B mount -t lustre /dev/sda1 /mnt/test/mdt
+.B mount -t lustre_tgt /dev/sda1 /mnt/test/mdt
 Start the Lustre metadata target service from
 .B /dev/sda1
 on mountpoint
 .BR /mnt/test/mdt .
 .TP
-.B mount -t lustre -L testfs-MDT0000 -o abort_recov /mnt/test/mdt
+.B mount -t lustre_tgt -L testfs-MDT0000 -o abort_recov /mnt/test/mdt
 Start the
 .B testfs-MDT0000
-service (by using the disk label), but aborts the recovery process if
-all of the clients are known to be unavailable.
+service (by using the disk label), but aborts the Lustre client recovery
+process for the case that clients are known to be unavailable.
 .SH BUGS
 Not very many mount options can be changed with
 .BR "-o remount" .

lustre/doc/mount.lustre_tgt.8
@@ -1,0 +1,1 @@
+.so man8/mount.lustre.8

lustre/llite/llite_lib.c
@@ -606,7 +606,8 @@ static int client_common_fill_super(struct super_block *sb, char *md, char *dt,
 #endif
 		err = IS_ERR(root) ? PTR_ERR(root) : -EBADF;
 		root = NULL;
-		CERROR("lustre_lite: bad iget4 for root\n");
+		CERROR("%s: bad ll_iget() for root: rc = %d\n",
+		       sbi->ll_fsname, err);
 		GOTO(out_root, err);
 	}
 

lustre/obdclass/obd_mount.c
@@ -1295,7 +1295,7 @@ static int lmd_parse_nidlist(char *buf, char **endh)
 /**
  * Parse mount line options
  * e.g. mount -v -t lustre -o abort_recov uml1:uml2:/lustre-client /mnt/lustre
- * dev is passed as device=uml1:/lustre by mount.lustre
+ * dev is passed as device=uml1:/lustre by mount.lustre_tgt
  */
 static int lmd_parse(char *options, struct lustre_mount_data *lmd)
 {
@@ -1308,14 +1308,14 @@ static int lmd_parse(char *options, struct lustre_mount_data *lmd)
 	LASSERT(lmd);
 	if (!options) {
 		LCONSOLE_ERROR_MSG(0x162,
-				   "Missing mount data: check /sbin/mount.lustre is installed.\n");
+				   "Missing mount data: check /sbin/mount.lustre_tgt is installed.\n");
 		RETURN(-EINVAL);
 	}
 
 	/* Options should be a string - try to detect old lmd data */
 	if ((raw->lmd_magic & 0xffffff00) == (LMD_MAGIC & 0xffffff00)) {
 		LCONSOLE_ERROR_MSG(0x163,
-				   "Using an old version of /sbin/mount lustre. Please install version %s\n",
+				   "Using an old version of /sbin/mount.lustre. Please install version %s\n",
 				   LUSTRE_VERSION_STRING);
 		RETURN(-EINVAL);
 	}
@@ -1727,26 +1727,72 @@ static void lustre_kill_super(struct super_block *sb)
 	kill_anon_super(sb);
 }
 
+#ifdef HAVE_SERVER_SUPPORT
+/* Register the "lustre_tgt" fs type.
+ *
+ * Right now this isn't any different than the normal "lustre" filesystem
+ * type, but it is added so that there is some compatibility to allow
+ * changing documentation and scripts to start using the "lustre_tgt" type
+ * at mount time. That will simplify test interop, and in case of upgrades
+ * that change to the new type and then need to roll back for some reason.
+ *
+ * The long-term goal is to disentangle the client and server mount code.
+ */
+static struct file_system_type lustre_fs_type_tgt = {
+	.owner		= THIS_MODULE,
+	.name		= "lustre_tgt",
+#ifdef HAVE_FSTYPE_MOUNT
+	.mount		= lustre_mount,
+#else
+	.get_sb		= lustre_get_sb,
+#endif
+	.kill_sb	= lustre_kill_super,
+	.fs_flags	= FS_REQUIRES_DEV | FS_RENAME_DOES_D_MOVE,
+};
+MODULE_ALIAS_FS("lustre_tgt");
+
+#define register_filesystem_tgt(fstype)					    \
+do {									    \
+	int _rc;							    \
+									    \
+	_rc = register_filesystem(fstype);				    \
+	if (_rc && _rc != -EBUSY) {					    \
+		/* Don't fail if server code also registers "lustre_tgt" */ \
+		CERROR("obdclass: register fstype '%s' failed: rc = %d\n",  \
+		       (fstype)->name, _rc);				    \
+		return _rc;						    \
+	}								    \
+} while (0)
+#define unregister_filesystem_tgt(fstype) unregister_filesystem(fstype)
+#else
+#define register_filesystem_tgt(fstype)   do {} while (0)
+#define unregister_filesystem_tgt(fstype) do {} while (0)
+#endif
+
 /* Register the "lustre" fs type */
 static struct file_system_type lustre_fs_type = {
-	.owner        = THIS_MODULE,
-	.name         = "lustre",
+	.owner		= THIS_MODULE,
+	.name		= "lustre",
 #ifdef HAVE_FSTYPE_MOUNT
-	.mount        = lustre_mount,
+	.mount		= lustre_mount,
 #else
-	.get_sb       = lustre_get_sb,
+	.get_sb		= lustre_get_sb,
 #endif
-	.kill_sb      = lustre_kill_super,
-	.fs_flags     = FS_REQUIRES_DEV | FS_RENAME_DOES_D_MOVE,
+	.kill_sb	= lustre_kill_super,
+	.fs_flags	= FS_REQUIRES_DEV | FS_RENAME_DOES_D_MOVE,
 };
 MODULE_ALIAS_FS("lustre");
 
 int lustre_register_fs(void)
 {
+	register_filesystem_tgt(&lustre_fs_type_tgt);
+
 	return register_filesystem(&lustre_fs_type);
 }
 
 int lustre_unregister_fs(void)
 {
+	unregister_filesystem_tgt(&lustre_fs_type_tgt);
+
 	return unregister_filesystem(&lustre_fs_type);
 }

lustre/utils/.gitignore
@@ -1,6 +1,7 @@
 /Makefile.in
 /mkfs.lustre
 /mount.lustre
+/mount.lustre_tgt
 /tunefs.lustre
 /lctl
 /lfs

commit e24d4d895d6bb5c0ecadd1aa8f8e5797db39a57a

LU-12514 tests: add testing for lustre_tgt type
Add test-framework handling for the lustre_tgt filesystem type
using /sbin/mount.lustre_tgt if it is installed on the servers.

Convert the core test-framework.sh code over to call $MOUNT_TGT
instead of $MOUNT_CMD or 'mount -t lustre'. Not all callers
have been converted yet, both to continue testing server mounts
with '-t lustre' as well as reducing the scope of this patch, as
each modified subtest triggers a review-subtest-change session.

Tests still using $MOUNT_CMD or 'mount -t lustre' can be updated
in a follow-on patch.

While mount.lustre_tgt has existed for a long time, installation
was broken in 2.17.0, so it cannot be tested with those servers
during interop sessions.

Fix lustre/utils/Makefile.am to properly install mount.lustre_tgt.
The $(SERVER) env variable was not set in the Makefile, use SERVER.
For Debian servers the $(DESTDIR)$(rootsbindir) was not created in
the install tree before the symlink was being created, do it first.
For RedHat servers using %ghost means the link was never installed.

Test-Parameters: trivial
Fixes: 510aea4a37 ("LU-12514 utils: add "lustre_tgt" filesystem type")
Fixes: 6493d8997e ("LU-17843 build: mount.lustre_tgt as symlink")
TLC-bug-id: TLU-95
Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
Signed-off-by: Minh Diep <mdiep@thelustrecollective.com>
Change-Id: I5b889b5bbb43475b702baab691deba6d333ebbe5

lustre/scripts/lustre_rmmod
@@ -109,15 +109,24 @@ else
 	unload_all=false
 fi
 
-if [ -f /sys/kernel/debug/kmemleak ] ; then
-	cat /proc/modules >/tmp/kmemleak-modules-list.txt
-	echo scan > /sys/kernel/debug/kmemleak
-	cat /sys/kernel/debug/kmemleak > /tmp/kmemleak-before-unload.txt
-	test -s /tmp/kmemleak-before-unload.txt && logger -t leak-pre -f /tmp/kmemleak-before-unload.txt
-	rm /tmp/kmemleak-before-unload.txt
-	# Clear everything here so that only new leaks show up
-	# after module unload
-	echo clear > /sys/kernel/debug/kmemleak
+export KMEMLEAK=${KMEMLEAK:-/sys/kernel/debug/kmemleak}
+KMEMLEAK_MODS=/tmp/kmemleak-modules-list.txt
+if [[ -w $KMEMLEAK ]]; then
+	if echo scan > $KMEMLEAK 2>&1; then
+		echo "kmemleak disabled"
+		export KMEMLEAK=disabled
+	else
+		kmemleak_pre=/tmp/kmemleak-pre-unload.txt
+
+		echo $kmemleak_out
+		cat /proc/modules > $KMEMLEAK_MODS
+		cat $KMEMLEAK > $kmemleak_pre
+		[[ -s $kmemleak_pre ]] && logger -t leak-pre -f $kmemleak_pre
+		rm -f $kmemleak_pre
+		# Clear everything here so that only new leaks show up
+		# after module unload
+		echo clear > $KMEMLEAK
+	fi
 fi
 
 # Manage debug

lustre/utils/Makefile.am
@@ -41,6 +41,7 @@ if SERVER
 sbin_PROGRAMS += mkfs.lustre tunefs.lustre lr_reader lshowmount \
 		 ll_decode_filter_fid llog_reader l_tunedisk \
 		 ofd_access_log_reader check_iam
+rootsbin_SCRIPTS = mount.lustre_tgt
 endif
 if LIBPTHREAD
 sbin_PROGRAMS += lhsmtool_posix
@@ -147,7 +148,7 @@ endif
 if SERVER
 LIB_TARGETS += mount_osd_wbcfs.so
 endif
-endif
+endif # PLUGINS
 
 install-exec-hook:
 	@$(NORMAL_INSTALL)
@@ -160,9 +161,10 @@ install-exec-hook:
 	    $(INSTALL_DATA) $$p $(DESTDIR)$(libdir)/lustre/$$f; \
 	  else :; fi; \
 	done
-	if [ "$(SERVER)" = "yes" ]; then \
-		ln -sf $(DESTDIR)$(sbindir)/mount.lustre $(DESTDIR)$(sbindir)/mount.lustre_tgt; \
-	fi
+if SERVER
+	$(mkinstalldirs) $(DESTDIR)$(rootsbindir)
+	ln -sf mount.lustre $(DESTDIR)$(rootsbindir)/mount.lustre_tgt
+endif # SERVER
 endif # UTILS
 
 llog_reader_SOURCES = llog_reader.c
@@ -258,6 +260,9 @@ mount_lustre_LDFLAGS  := ${MNTMODLDFLAGS}
 mount_lustre_LDADD    := $(SELINUX) $(LDLIBMOUNT) $(PLUGIN_LIB) $(GSSLIB)
 mount_lustre_LDADD    += $(top_builddir)/libcfs/libcfs/libcfs.la $(MNTMODLIBS)
 mount_lustre_LDADD    += liblustreapi.la
+
+mount.lustre_tgt: mount.lustre
+	ln -sf mount.lustre mount.lustre_tgt
 
 mkfs_lustre_SOURCES = mkfs_lustre.c mount_utils.c mount_utils.h $(GSSSRC)
 mkfs_lustre_CPPFLAGS := -UTUNEFS ${MNTMODCFLAGS}

lustre.spec.in
@@ -1039,7 +1039,7 @@ echo '%{_libdir}/lustre/tests/lutf/*' >>lustre-tests.files
 %{_bindir}/lfs_migrate
 /sbin/mount.lustre
 %if %{with servers}
-%ghost /sbin/mount.lustre_tgt
+/sbin/mount.lustre_tgt
 %endif
 @BASH_COMPLETION_DIR@
 %endif

lustre/tests/conf-sanity.sh
@@ -2200,7 +2200,7 @@ t32_test() {
 		if [ "$mds1_FSTYPE" == ldiskfs ]; then
 			mopts="loop,$mopts"
 		fi
-		$r $MOUNT_CMD -o $mopts $mdt_dev $tmp/mnt/mdt
+		$r $MOUNT_TGT -o $mopts $mdt_dev $tmp/mnt/mdt
 		$r $LCTL replace_nids $fsname-OST0000 $ostnid || {
 			$r $LCTL dl
 			error_noexit "replace_nids $fsname-OST0000 $ostnid failed"
@@ -2235,7 +2235,7 @@ t32_test() {
 
 	t32_wait_til_devices_gone $node
 
-	$r $MOUNT_CMD -o $mopts $mdt_dev $tmp/mnt/mdt || {
+	$r $MOUNT_TGT -o $mopts $mdt_dev $tmp/mnt/mdt || {
 		$r losetup -a
 		error_noexit "Mounting the MDT"
 		return 1
@@ -2245,7 +2245,7 @@ t32_test() {
 	if $mdt2_is_available; then
 		echo "== mdt2 available =="
 		mopts=mgsnode=$nid,$mopts
-		$r $MOUNT_CMD -o $mopts $mdt2_dev $tmp/mnt/mdt1 || {
+		$r $MOUNT_TGT -o $mopts $mdt2_dev $tmp/mnt/mdt1 || {
 			$r losetup -a
 			error_noexit "Mounting the MDT"
 			return 1
@@ -2282,7 +2282,7 @@ t32_test() {
 		}
 
 		echo "== mount new MDT....$fs2mdsdev =="
-		$r $MOUNT_CMD -o $mopts $fs2mdsdev $tmp/mnt/mdt1 || {
+		$r $MOUNT_TGT -o $mopts $fs2mdsdev $tmp/mnt/mdt1 || {
 			error_noexit "mount mdt1 failed"
 			return 1
 		}
@@ -2358,13 +2358,13 @@ t32_test() {
 		fi
 	fi
 
-	$r $MOUNT_CMD -onomgs -o$mopts $ost_dev $tmp/mnt/ost || {
+	$r $MOUNT_TGT -onomgs -o$mopts $ost_dev $tmp/mnt/ost || {
 		error_noexit "Mounting the OST"
 		return 1
 	}
 
 	if $ost2_is_available; then
-		$r $MOUNT_CMD -onomgs -o$mopts $ost2_dev $tmp/mnt/ost1 || {
+		$r $MOUNT_TGT -onomgs -o$mopts $ost2_dev $tmp/mnt/ost1 || {
 			error_noexit "Mounting the OST2"
 			return 1
 		}
@@ -3164,7 +3164,7 @@ t32_test() {
 		if [ "$mds1_FSTYPE" == ldiskfs ]; then
 			mopts="loop,$mopts"
 		fi
-		$r $MOUNT_CMD -o $mopts $mdt_dev $tmp/mnt/mdt || {
+		$r $MOUNT_TGT -o $mopts $mdt_dev $tmp/mnt/mdt || {
 			error_noexit "Remounting the MDT"
 			return 1
 		}
@@ -3818,8 +3818,8 @@ test_37() {
 	fi
 
 	load_modules
-	mount_op=$(do_facet $SINGLEMDS mount -v -t lustre $opts \
-		$mdsdev_sym $mntpt 2>&1)
+	mount_op=$(do_facet $SINGLEMDS $MOUNT_TGT -v $opts \
+		   $mdsdev_sym $mntpt 2>&1)
 	rc=${PIPESTATUS[0]}
 
 	echo mount_op=$mount_op
@@ -4030,10 +4030,10 @@ test_41c() {
 	#define OBD_FAIL_TGT_MOUNT_RACE 0x716
 	do_facet mds1 "$LCTL set_param fail_loc=0x80000716"
 
-	do_facet mds1 mount -t lustre $mds1dev $mds1mnt $mds1opts &
+	do_facet mds1 $MOUNT_TGT $mds1dev $mds1mnt $mds1opts &
 	local pid=$!
 
-	do_facet mds1 mount -t lustre $mds1dev $mds1mnt $mds1opts
+	do_facet mds1 $MOUNT_TGT $mds1dev $mds1mnt $mds1opts
 	local rc2=$?
 	wait $pid
 	local rc=$?
@@ -4076,10 +4076,10 @@ test_41c() {
 	#define OBD_FAIL_TGT_MOUNT_RACE 0x716
 	do_facet ost1 "$LCTL set_param fail_loc=0x80000716"
 
-	do_facet ost1 mount -t lustre $ost1dev $ost1mnt $ost1opts &
+	do_facet ost1 $MOUNT_TGT $ost1dev $ost1mnt $ost1opts &
 	pid=$!
 
-	do_facet ost1 mount -t lustre $ost1dev $ost1mnt $ost1opts
+	do_facet ost1 $MOUNT_TGT $ost1dev $ost1mnt $ost1opts
 	rc2=$?
 	wait $pid
 	rc=$?

lustre/tests/test-framework.sh
@@ -36,6 +36,7 @@ export JOBID_VAR=${JOBID_VAR:-"procname_uid"}  # or "existing" or "disable"
 
 #export PDSH="pdsh -S -Rssh -w"
 export MOUNT_CMD=${MOUNT_CMD:-"mount -t lustre"}
+export MOUNT_TGT=${MOUNT_TGT:-"mount -t lustre_tgt"}
 export UMOUNT=${UMOUNT:-"umount -d"}
 
 # A switch to enable kptr less restrictively
@@ -1029,10 +1030,12 @@ load_lnet() {
 	# For kmemleak-enabled kernels we need clear all past state
 	# that obviously has nothing to do with this Lustre run
 	# Disable automatic memory scanning to avoid perf hit.
-	if [ -f /sys/kernel/debug/kmemleak ] ; then
-		echo scan=off > /sys/kernel/debug/kmemleak || true
-		echo scan > /sys/kernel/debug/kmemleak || true
-		echo clear > /sys/kernel/debug/kmemleak || true
+	if [[ -w $KMEMLEAK ]] ; then
+		if echo scan=off > $KMEMLEAK || echo scan > $KMEMLEAK
+		   echo clear > $KMEMLEAK || true; then
+			echo "kmemleak disabled"
+			export KMEMLEAK=disabled
+		fi
 	fi
 
 	echo Loading modules from $LUSTRE
@@ -1189,16 +1192,20 @@ load_modules_local() {
 	rm -f $OGDB/ogdb-$HOSTNAME
 	$LCTL modules > $OGDB/ogdb-$HOSTNAME
 
-	# 'mount' doesn't look in $PATH, just sbin
-	local mount_lustre=$LUSTRE/utils/mount.lustre
-	if [ -f $mount_lustre ]; then
-		local sbin_mount=$(readlink -f /sbin)/mount.lustre
+	# 'mount' doesn't look in $PATH, it hard-codes /sbin/mount.$FSTYPE
+	local mount_lustre
+	for mount_lustre in $LUSTRE/utils/mount.{lustre,lustre_tgt}; do
+		local sbin_mount=$(readlink -f /sbin)/$(basename $mount_lustre)
+		ls -l $sbin_mount $mount_lustre || true
+		[[ -f $mount_lustre ]] || continue
+		! client_only || [[ ! $mount_lustre =~ tgt ]] || continue
+
 		if grep -qw "$sbin_mount" /proc/mounts; then
 			cmp -s $mount_lustre $sbin_mount || umount $sbin_mount
 		fi
 		if ! grep -qw "$sbin_mount" /proc/mounts; then
-			[ ! -f "$sbin_mount" ] && touch "$sbin_mount"
-			if [ ! -s "$sbin_mount" -a -w "$sbin_mount" ]; then
+			[[ -f "$sbin_mount" ]] || touch "$sbin_mount" || continue
+			if [[ ! -s "$sbin_mount" && -w "$sbin_mount" ]]; then
 				cat <<- EOF > "$sbin_mount"
 				#!/bin/bash
 				#STUB MARK
@@ -1209,13 +1216,22 @@ load_modules_local() {
 				EOF
 				chmod a+x $sbin_mount
 			fi
+			echo "mount --bind $mount_lustre $sbin_mount"
 			mount --bind $mount_lustre $sbin_mount ||
 				error "can't bind $mount_lustre to $sbin_mount"
 			# ignore errors to symlink .libs for read-only /sbin
-			[[ -e /sbin/.libs ]] ||
+			[[ ! -w /sbin || -e /sbin/.libs ]] ||
 				ln -sf $LUSTRE/utils/.libs /sbin/.libs || true
 		fi
-	fi
+	done
+
+	# /sbin/mount.lustre wasn't installed as part of 2.17, nor in Janitor
+	# this may be run before $MDS1_VERSION is set, check version directly
+	local mds1_version=$(lustre_version_code mds1)
+	(( $mds1_version > $(version_code 2.17.53.0) ||
+	   $mds1_version < $(version_code v2_16_53-7-g6493d8997e) )) &&
+	[[ ! $HOSTNAME =~ oleg ]] ||
+		MOUNT_TGT=$MOUNT_CMD
 }
 
 load_modules () {
@@ -2572,7 +2588,7 @@ mount_facet() {
 
 		case $fstype in
 		wbcfs)
-			echo "Start ${facet}: $MOUNT_CMD -v lustre-wbcfs $mntpt"
+			echo "Start ${facet}: $MOUNT_TGT -v lustre-wbcfs $mntpt"
 
 			export OSD_WBC_FSNAME="$FSNAME"
 			export OSD_WBC_INDEX="$index"
@@ -2604,12 +2620,12 @@ mount_facet() {
 				 OSD_WBC_MGS_NID=$OSD_WBC_MGS_NID \
 				 OSD_WBC_PRIMARY_MDT=$OSD_WBC_PRIMARY_MDT \
 				 OSD_WBC_FSNAME=$OSD_WBC_FSNAME \
-				 $MOUNT_CMD -v lustre-wbcfs $mntpt"
+				 $MOUNT_TGT -v lustre-wbcfs $mntpt"
 			;;
 		*)
-			echo "Start ${facet}: $MOUNT_CMD $opts $dm_dev $mntpt"
+			echo "Start ${facet}: $MOUNT_TGT $opts $dm_dev $mntpt"
 			do_facet ${facet} \
-				"mkdir -p $mntpt; $MOUNT_CMD $opts $dm_dev $mntpt"
+				"mkdir -p $mntpt; $MOUNT_TGT $opts $dm_dev $mntpt"
 		esac
 
 		RC=${PIPESTATUS[0]}